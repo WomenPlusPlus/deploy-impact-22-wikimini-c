0f5a8c7debe5b1515971e3de6e903bc447d8c6b5
vasara1987 <you@example.com>
2022-10-21 20:50:32 +0200

    Create add class page and add student modal

---
+++ wikimini-frontend/src/pages/add-class-page/add-student-modal.jsx
@@ -0,0 +1,161 @@
+import React from 'react';
+import {
+  Box,
+  Typography,
+  TextField,
+  Modal,
+  Button,
+} from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import ModalButton from '../../components/modal-button';
+
+const initialValues = {
+  firstName: '',
+  surname: '',
+  username: '',
+  password: '',
+};
+
+const validationSchema = yup.object({
+  firstName: yup.string()
+    .required('Required')
+    .min(2, 'At least 2 characters')
+    .matches(/^[a-ząčęėįšųūž ]+$/i, 'Only letters and spaces'),
+  surname: yup.string()
+    .required('Required')
+    .min(2, 'At least 2 characters')
+    .matches(/^[a-ząčęėįšųūž ]+$/i, 'Only letters and spaces'),
+  username: yup.string()
+    .required('Required')
+    .min(2, 'At least 2 characters')
+    .matches(/^[a-ząčęėįšųūž ]+$/i, 'Only letters and spaces'),
+  password: yup.string()
+    .required('Required')
+    .min(8, 'At least 8 characters')
+    .matches(/[a-z]/, 'At least one upper-case letter')
+    .matches(/[A-Z]/, 'At least one lower-case letter')
+    .matches(/\d/, 'At least one number')
+    .matches(/\W/, 'At least one special symbol'),
+});
+
+const AddStudentModal = () => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const onSubmit = (values) => {
+    console.log('Values entered');
+    console.table(values);
+  };
+
+  const {
+    dirty, values, errors, touched, isValid, handleChange, handleBlur, handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Box>
+      <ModalButton openModal={() => setModalOpen(true)}>ADD A STUDENT</ModalButton>
+      <Modal open={modalOpen} onClose={closeModal}>
+        <Box sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            height="600px"
+            width="500px"
+            backgroundColor="white"
+            p={7}
+            gap={3}
+            justifyContent="center"
+            alignItems="center"
+          >
+            {' '}
+            <Typography mb={3}>ADD A STUDENT</Typography>
+            <Box
+              component="form"
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 3,
+                width: '100%',
+              }}
+              onSubmit={handleSubmit}
+            >
+              <TextField
+                name="firstName"
+                label="Name"
+                type="text"
+                variant="filled"
+                fullWidth
+                value={values.firstName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.firstName && Boolean(errors.firstName)}
+                helperText={touched.firstName && errors.firstName}
+              />
+              <TextField
+                name="surname"
+                label="Surname"
+                type="text"
+                variant="filled"
+                fullWidth
+                value={values.surname}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.surname && Boolean(errors.surname)}
+                helperText={touched.surname && errors.surname}
+              />
+              <TextField
+                name="surname"
+                label="Username"
+                type="text"
+                variant="filled"
+                fullWidth
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.username && Boolean(errors.username)}
+                helperText={touched.username && errors.username}
+              />
+              <TextField
+                name="password"
+                label="Password"
+                type="password"
+                variant="filled"
+                fullWidth
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={touched.password && Boolean(errors.password)}
+                helperText={touched.password && errors.password}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                size="large"
+                disabled={!dirty || !isValid}
+              >
+                Add
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
+};
+
+export default AddStudentModal;
