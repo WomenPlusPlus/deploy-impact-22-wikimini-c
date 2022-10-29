3ff82cc19b9b23d832131fa9fda9b525393a885b
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-10-29 19:20:47 -0300

    Remove try catch to handle it in the action

--- src/services/teacher.js
+++ src/services/teacher.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
 
 export const createClassService = async (classData) => {
-  try {
-    const url = `${process.env.REACT_APP_API_URL}api.php?action=createclass&class_name=${classData.classTitle}&format=json`;
-    const response = await axios.post(url, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(response);
+  const url = `${process.env.REACT_APP_API_URL}api.php?action=createclass&class_name=${classData.classTitle}&format=json`;
+  const response = await axios.post(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return response;
-  } catch (error) {
-    console.log(error);
-
-    return error;
-  }
+  return response;
 };
