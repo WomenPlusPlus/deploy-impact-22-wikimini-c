5ae90d19ad0751fcc0fe1aa38aef97e3ee2de531
vasara1987 <you@example.com>
2022-11-02 18:47:13 +0100

    Categories and about us section layout

--- src/pages/home-page/home.js
+++ src/pages/home-page/home.js
@@ -15,7 +15,7 @@ const HomePage = () => (
       </form>
       <div className="description-container">
         <div className="description-box">
-          <p className="title">Welcome to Wikimini!</p>
+          <h1>Welcome to Wikimini!</h1>
           <p>
             Wikimini is an online encyclopaedia written by children and teenagers,
             with the help of older people.
@@ -26,6 +26,79 @@ const HomePage = () => (
         </div>
         <div className="image-child" />
         <div className="image-item" />
+      </div>
+    </div>
+    <div className="category-container">
+      <p className="title">SEARCH BY CATEGORY</p>
+      <div className="grid-container">
+        <div className="category-card">
+          <img className="category-image" src="Atlas_Of_The_World.png" alt="category" />
+          <p>Atlas of the world</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="artsandsports.png" alt="category" />
+          <p>Arts, leisure and sports</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Geography.png" alt="category" />
+          <p>History and geography</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Information.png" alt="category" />
+          <p>Information and documentation</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Languages.png" alt="category" />
+          <p>Languages</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="LifeInSociety.png" alt="category" />
+          <p>Life in society</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Mind.png" alt="category" />
+          <p>Thought and mind</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Religions.png" alt="category" />
+          <p>Beliefs and religions</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Science.png" alt="category" />
+          <p>Science and mathematics</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Theater.png" alt="category" />
+          <p>Literature and theater</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="TechnologyandHealth.png" alt="category" />
+          <p>Technology and health</p>
+        </div>
+        <div className="category-card">
+          <img className="category-image" src="Question.png" alt="category" />
+          <p>Quiz</p>
+        </div>
+
+      </div>
+    </div>
+    <div className="about-us-container">
+      <div>
+        <img className="about-us-image" src="ImageAboutUs.png" alt="people" />
+
+      </div>
+      <div>
+        <p className="title">ABOUT US</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel dolor commodo,
+          condimentum lacus vel, interdum nisi. Quisque eu magna quis nisi accumsan molestie
+          eget at elit. Sed maximus ligula et libero ornare semper. Phasellus ac efficitur
+          sem, cursus feugiat tortor. Aenean condimentum sit amet lacus eu varius.
+          Pellentesque ultricies non urna et vehicula. Nulla nec laoreet velit, et aliquam
+          est.
+          {' '}
+
+        </p>
       </div>
     </div>
 
