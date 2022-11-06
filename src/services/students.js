91c93dd14ae9c8f3077a70935be1068a1ec9a3a2
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-11-06 12:56:19 -0300

    Add add student service

---
+++ src/services/students.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const addStudentService = (student) => {
+  // The same student cannot be added to the same class - this will result in a DB error.
+  // If the student and/or class does not exist a error will be displayed.
+
+  const url = `${process.env.REACT_APP_API_URL}api.php`;
+
+  const params = {
+    action: 'addstudent',
+    format: 'json',
+  };
+
+  const formData = new FormData();
+  formData.append('student_name', student.username);
+  formData.append('class_id', student.classId);
+
+  return axios.post(url, formData, { params });
+};
+
+// TODO: addStudentToTaskService
+
+// TODO: list students for a given class
