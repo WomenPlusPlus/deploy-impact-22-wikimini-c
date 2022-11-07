0a9aaa59aa1d7aa6fb41035c8a96e794023a260b
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-11-06 21:40:30 -0300

    Add student list action to assign students page

--- src/pages/assign-student-page/index.jsx
+++ src/pages/assign-student-page/index.jsx
@@ -1,53 +1,51 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Box, List, ListItem, ListItemText, Divider, Typography,
 } from '@mui/material';
 import MainContainer from '../../components/main-container';
+import { listStudentsInClass } from '../../redux/reducers/student';
 
 const style = {
   width: '100%',
   bgcolor: 'background.paper',
 };
 
-const students = [
-  { id: 1, studentName: 'Student1' },
-  { id: 2, studentName: 'Student2' },
-  { id: 3, studentName: 'Student3' },
-  { id: 4, studentName: 'Student4' },
-  { id: 5, studentName: 'Student5' },
-  { id: 6, studentName: 'Student6' },
-  { id: 7, studentName: 'Student7' },
-  { id: 8, studentName: 'Student8' },
-  { id: 9, studentName: 'Student9' },
-  { id: 10, studentName: 'Student10' },
-];
+const AssignStudentPage = () => {
+  const dispatch = useDispatch();
+  const students = useSelector((state) => state.student.data.students);
+  console.log(students);
+  React.useEffect(() => {
+    dispatch(listStudentsInClass(1));
+  }, [dispatch]);
 
-const AssignStudentPage = () => (
-  <MainContainer>
-    <Typography variant="h5">Select Students</Typography>
-    {' '}
-    <Box width="100%" border="1px solid black" borderRadius={1} mb={5}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-        }}
-      >
-        <List sx={style} component="nav">
-          {students.map(({ id, studentName }) => (
-            <div key={id}>
-              <ListItem>
-                <ListItemText key={studentName} primary={studentName} />
-              </ListItem>
-              <Divider variant="middle" />
-            </div>
-          ))}
-        </List>
+  return (
+    <MainContainer>
+      <Typography variant="h5">Select Students</Typography>
+      {' '}
+      <Box width="100%" border="1px solid black" borderRadius={1} mb={5}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+          }}
+        >
+          <List sx={style} component="nav">
+            {students.map(({ id, username }) => (
+              <div key={id}>
+                <ListItem>
+                  <ListItemText key={username} primary={username} />
+                </ListItem>
+                <Divider variant="middle" />
+              </div>
+            ))}
+          </List>
+        </Box>
       </Box>
-    </Box>
 
-  </MainContainer>
-);
+    </MainContainer>
+  );
+};
 
 export default AssignStudentPage;
