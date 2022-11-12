86533206b378bc18019c45233349e889cee2813d
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-11-11 23:47:58 -0300

    Add class dashboard endpoint and update pages

--- src/services/classes.js
+++ src/services/classes.js
@@ -33,3 +33,49 @@ export const createTaskService = async (taskData) => {
 
   return axios.post(url, formData, { params });
 };
+
+export const getClassDetailsService = async (classId) => {
+  const url = `${process.env.REACT_APP_API_URL}api.php`;
+
+  const params = {
+    action: 'query',
+    list: 'getclassdetails',
+    class_id: classId,
+    format: 'json',
+  };
+
+  const response = await axios.get(url, { params });
+  const result = response.data.query.getclassdetails[0];
+
+  console.log(result);
+
+  const allStudents = Object.keys(result.all_students).map((key) => ({
+    id: result.all_students[key].student_id,
+    studentUsername: result.all_students[key].student_username,
+  }));
+
+  const allTasks = Object.keys(result.all_tasks).map((key) => ({
+    id: result.all_tasks[key].task_id,
+    task: result.all_tasks[key].task_name,
+  }));
+
+  const tasksPerStudent = Object.keys(result.tasks_per_student).map(
+    (key, id) => ({
+      id,
+      studentId: result.tasks_per_student[key].student_id,
+      studentUsername: result.tasks_per_student[key].student_username,
+      taskId: result.tasks_per_student[key].task_id,
+      task: result.tasks_per_student[key].task_name,
+      taskStatus: result.tasks_per_student[key].task_status,
+    })
+  );
+
+  const classInfo = result.class_info;
+
+  return {
+    allStudents,
+    allTasks,
+    tasksPerStudent,
+    classInfo,
+  };
+};
